d47906acee2117936ce691638f262fab5733f4fb
Zyad Galal <zyad_mg@yahoo.com>
2019-05-04 15:36:08 +0200

    Initial Commit

---
+++ BlindIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlindIn
+//
+//  Created by Zyad Galal on 5/4/19.
+//  Copyright © 2019 Zyad Galal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
